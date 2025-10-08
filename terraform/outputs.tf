25ba452a77c215e17e6722e2961f5c729e865604
Abioye <abioye.sokunbi@gmail.com>
2025-10-08 23:07:34 +0100

    Auto changes

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,3 @@
-output "vpc_id"        { value = aws_vpc.main.id }
-output "alb_dns_name"  { value = aws_lb.app_alb.dns_name }
-output "db_endpoint"   { value = aws_db_instance.app_db.address }
+output "vpc_id" { value = aws_vpc.main.id }
+output "alb_dns_name" { value = aws_lb.app_alb.dns_name }
+output "db_endpoint" { value = aws_db_instance.app_db.address }
