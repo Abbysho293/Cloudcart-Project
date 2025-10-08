a474fe2494b93a3efc5440d0b2f7d4023f72596b
Abioye <abioye.sokunbi@gmail.com>
2025-10-08 23:12:30 +0100

    Included variables for secrets manager

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,19 +1,67 @@
-variable "aws_region"           { type = string  default = "us-east-1" }
-variable "project_name"         { type = string  default = "cloudcart" }
-variable "vpc_cidr"             { type = string  default = "10.0.0.0/16" }
-variable "public_subnet_cidrs"  { type = list(string) default = ["10.0.1.0/24", "10.0.2.0/24"] }
-variable "private_subnet_cidrs" { type = list(string) default = ["10.0.11.0/24", "10.0.12.0/24"] }
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
 
-variable "db_username"          { type = string  default = "cloudcart" }
-variable "db_password"          { type = string  sensitive = true }
-variable "db_instance_class"    { type = string  default = "db.t3.micro" }
-variable "db_allocated_storage" { type = number  default = 20 }
-variable "db_multi_az"          { type = bool    default = false }
+variable "project_name" {
+  type    = string
+  default = "cloudcart"
+}
 
-variable "app_port"             { type = number  default = 3000 }
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+variable "public_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+variable "private_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.0.11.0/24", "10.0.12.0/24"]
+}
 
-variable "desired_capacity"     { type = number  default = 2 }
-variable "min_size"             { type = number  default = 1 }
-variable "max_size"             { type = number  default = 3 }
+variable "db_username" {
+  type    = string
+  default = "cloudcart"
+}
+#variable "db_password" {
+ # type      = string
+ # sensitive = true
+#}
+variable "db_instance_class" {
+  type    = string
+  default = "db.t3.micro"
+}
+variable "db_allocated_storage" {
+  type    = number
+  default = 20
+}
+variable "db_multi_az" {
+  type    = bool
+  default = false
+}
 
-variable "dockerhub_repo"       { type = string }
+variable "app_port" {
+  type    = number
+  default = 3000
+}
+
+variable "desired_capacity" {
+  type    = number
+  default = 2
+}
+
+variable "min_size" {
+  type    = number
+  default = 1
+}
+
+variable "max_size" {
+  type    = number
+  default = 3
+}
+
+variable "dockerhub_repo" {
+  type = string
+}
